f5e7d0afbce9fd18f9338f225faf0cef11368627
AaqibhafeezKhan <aaqibhafeezkhan@gmail.com>
2026-04-17 00:41:57 +0530

    feat: implement Add function with support for custom delimiters, negative number validation, and ignoring values over 1000

--- calculator.js
+++ calculator.js
@@ -1,7 +1,33 @@
+/**
+ * Escapes special characters in a string for use in creating a Regular Expression.
+ * @param {string} str - The input string (delimiter).
+ * @returns {string} - The string with special regex characters escaped.
+ */
 function escapeRegex(str) {
   return str.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
 }
 
+/**
+ * String Calculator Add Function - Steps 1-9
+ *
+ * Takes a string containing numbers and returns their sum.
+ * - Supports default delimiters: comma (,) and newline (\n).
+ * - Supports custom delimiters defined via an optional prefix: "//[delimiter]\n[numbers...]".
+ * - Delimiters can be of any length with the format: "//[delimiter]\n".
+ * - Allows multiple delimiters like this: “//[delim1][delim2]\n”.
+ * - Handles multiple delimiters with length longer than one char.
+ * - An empty string returns 0.
+ * - Handles any amount of numbers.
+ * - Calling Add with a negative number will throw an exception “negatives not allowed”
+ * - and the negative that was passed.
+ * - If there are multiple negatives, show all of them in the exception message.
+ * - Numbers bigger than 1000 should be ignored.
+ * - Assumes valid input sequences otherwise.
+ *
+ * @param {string} numbers - The input string.
+ * @returns {number} - The calculated sum.
+ * @throws {Error} - If negative numbers are encountered, with a message "negatives not allowed"
+ */
 export function Add(numbers) {
   if (numbers === "") {
     return 0;
